7f1dd21d96a593159b9b44f85f68f68846a4f362
Chi Suhua <chisuhua@gmail.com>
2022-02-21 23:45:43 +0800

    update build.sh

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+
+set -e
+
+build_dbg() {
+	rm -rf build && mkdir build && cd build && meson .. && ninja || exit 1
+}
+
+build_dbg() {
+	rm -rf build && mkdir build && cd build && meson .. --buildtype release && ninja || exit 1
+}
+
+if [ ! -z "$1" ]; then
+	if [ $1 == lint ]; then
+		echo start link check ...
+	elif [ $1 == dbg ]; then
+		build_dbg;
+	elif [ $1 == rel ]; then
+		build_release;
+	fi
+else
+	build_dbg;
+fi
+
